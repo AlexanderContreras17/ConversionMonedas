b240a5f7a1e406015c91f7f21956f9615bdc931f
AlexanderContreras17 <hatakealexander24@gmail.com>
2023-09-29 10:17:29 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using ConversionMoneda.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConversionMoneda.Controllers
+{
+    public class HomeController : Controller
+	{
+		//pesos- cantidad
+		//conversion-resultado
+		//moneda -tipo
+		public IActionResult Index(IndexViewModel vm)
+		{
+			if(vm.Moneda == "mx")
+			{
+				vm.conversion = vm.pesos / 18m;
+			}
+			else if(vm.Moneda == "us")
+			{
+				vm.conversion = vm.pesos * 18m;
+			}
+			return View(vm);
+		}
+	}
+}
---
+++ Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ConversionMoneda.Models.ViewModels
+{
+    public class IndexViewModel
+    {
+        public decimal pesos { get; set; }
+        public decimal conversion { get; set; }
+        public string Moneda { get; set; } = null!;
+
+    }
+}
